9d4749fcff90e9b4879d51caf302f05beaf8b564
유진이 <ssafy_coach_52@ssafy.com>
2021-09-28 14:33:04 +0900

    boj 20056 시간초과 해결

--- 문제풀이/Backjoon/BOJ_20056_마법사상어와파이어볼.java
+++ 문제풀이/Backjoon/BOJ_20056_마법사상어와파이어볼.java
@@ -3,14 +3,19 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+// map에 ball의 개수를 기록하고, ballList를 따로 관리하는 방법 => 시간초과..
+// map 한칸을 list로 생성해서 모든 ballList를 돌지말고
+// 그 칸의 ballList만 돌면서 확인하도록 수정 -> 해결
+
 public class BOJ_20056_마법사상어와파이어볼 {
     static StringTokenizer st;
     static int N, M, K, ANS;
-    static int[][] map;
+    static ArrayList<Ball>[][] map;
     static Queue<Ball> ballList;
     static int[] di = {-1,-1,0,1,1,1,0,-1};
     static int[] dj = {0,1,1,1,0,-1,-1,-1};
@@ -20,7 +25,12 @@ public class BOJ_20056_마법사상어와파이어볼 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
+        map = new ArrayList[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                map[i][j] = new ArrayList<>();
+            }
+        }
         ballList = new LinkedList<>();
         for (int i = 0; i < M; i++) { // 처음 공 위치 표시
             st = new StringTokenizer(br.readLine());
@@ -29,71 +39,58 @@ public class BOJ_20056_마법사상어와파이어볼 {
             int m = Integer.parseInt(st.nextToken());
             int s = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
-            map[r-1][c-1]++;
             ballList.add(new Ball(r-1, c-1, m, s, d));
         }
 
         for (int i = 0; i < K; i++) {
             int s = ballList.size();
+            // 1번부터 N번까지 이동
             for (int size = 0; size < s; size++) {
                 Ball now = ballList.poll();
+
                 int sr = now.r; int sc = now.c;
-                map[sr][sc]--;
                 int nr = sr + di[now.d] * now.s % N;
                 int nc = sc + dj[now.d] * now.s % N;
                 if(nr >= N ) nr -= N;
                 if(nr < 0 ) nr += N;
                 if(nc >= N ) nc -= N;
                 if(nc < 0 ) nc += N;
-                map[nr][nc]++;
-                ballList.add(new Ball(nr, nc, now.m, now.s, now.d));
+                map[nr][nc].add(new Ball(nr, nc, now.m, now.s, now.d));
             }
             // 이동하고 2개이상이면 같은 칸에 4개로 만들어주기
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    if(map[j][k] >= 2){
-                        int cnt = map[j][k]; int mSum = 0; int sSum = 0;
+                    if(map[j][k].size() == 1) {
+                        ballList.add(map[j][k].get(0));
+                    }
+                    else if(map[j][k].size() >= 2){
+                        int cnt = map[j][k].size(); int mSum = 0; int sSum = 0;
                         int dirL = 0; int dirR = 0;
-                        map[j][k] = 0;
-                        int size = ballList.size();
-                        for (int l = 0; l < size; l++) {
-                            Ball ball = ballList.poll();
-                            if(ball.r == j && ball.c == k){
-                                mSum += ball.m;
-                                sSum += ball.s;
-                                if(ball.d % 2 == 0) dirL++;
-                                else dirR++;
-                            }else{
-                                ballList.add(ball);
-                            }
+                        for (int l = 0; l < cnt; l++){
+                            mSum += map[j][k].get(l).m;
+                            sSum += map[j][k].get(l).s;
+                            if(map[j][k].get(l).d % 2 == 0) dirL++;
+                            else dirR++;
                         }
-                        map[j][k] = 4; // 4개로 나눔
-                        if(dirL == 0 || dirR == 0){
-                            for (int dir = 0; dir < 8; dir+=2) {
-                                if(mSum/5 != 0) ballList.add(new Ball(j, k, mSum/5, sSum/cnt,dir));
-                            }
-                        }else{
-                            for (int dir = 1; dir < 8; dir+=2) {
-                                if(mSum/5 != 0) ballList.add(new Ball(j, k, mSum/5, sSum/cnt,dir));
+
+                        if(mSum/5 != 0){
+                            if(dirL == 0 || dirR == 0){
+                                for (int dir = 0; dir < 8; dir+=2) {
+                                    ballList.add(new Ball(j, k, mSum/5, sSum/cnt,dir));
+                                }
+                            }else {
+                                for (int dir = 1; dir < 8; dir += 2) {
+                                    ballList.add(new Ball(j, k, mSum / 5, sSum / cnt, dir));
+                                }
                             }
                         }
                     }
+                    map[j][k].clear();
                 }
             }
         }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(map[i][j] > 0){
-                    int s = ballList.size();
-                    for (int k = 0; k < s; k++) {
-                        Ball ball = ballList.poll();
-                        if(ball.r == i && ball.c == j){
-                            ANS += ball.m;
-                        }else ballList.add(ball);
-                    }
-                }
-            }
+        for (Ball ball:ballList) {
+            ANS += ball.m;
         }
         System.out.println(ANS);
     }
@@ -107,16 +104,5 @@ public class BOJ_20056_마법사상어와파이어볼 {
             this.s = s;
             this.d = d;
         }
-    }
-
-    static void print(){
-        System.out.println("-------------------------");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(map[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println("-------------------------");
     }
 }
